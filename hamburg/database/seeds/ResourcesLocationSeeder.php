34fa177734a035039126b5415f1f47cc150d0bab
Alejandro <a13xemz@gmail.com>
2017-10-14 06:06:08 -0700

    HOTFIX

--- hamburg/database/seeds/ResourcesLocationSeeder.php
+++ hamburg/database/seeds/ResourcesLocationSeeder.php
@@ -23,7 +23,7 @@ class ResourcesLocationSeeder extends Seeder
         	]);
         	$location = Location::create([
         		'lat' => rand(34.20*1000000, 34.22*1000000) / 1000000,
-            'lng' => rand((-118.525)*1000000, (-118.535)*1000000) / 1000000,
+                'lng' => rand((-118.525)*1000000, (-118.54)*1000000) / 1000000,
         		'source_type' => 'resources'
         	]);
         	$resource = Resource::create([
@@ -31,6 +31,25 @@ class ResourcesLocationSeeder extends Seeder
         		'reporter' => $distress->id,
         		'location_id' => $location->id
         	]);
+        }
+        
+        $type = ['water','power','shelter','zombie'];
+        for ($i=0; $i < 20; $i++) { 
+            $distress = Distress::create([
+                'name' => 'Fake',
+                'phone_number'=> '123456789',
+                'safe' => rand(0,1)
+            ]);
+            $location = Location::create([
+                'lat' => rand(33.88*1000000, 33.95*1000000) / 1000000,
+            'lng' => rand((-118.36)*1000000, (-118.38)*1000000) / 1000000,
+                'source_type' => 'resources'
+            ]);
+            $resource = Resource::create([
+                'type' => array_random($type),
+                'reporter' => $distress->id,
+                'location_id' => $location->id
+            ]);
         }
     }
 }
