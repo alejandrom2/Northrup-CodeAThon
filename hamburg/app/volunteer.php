7b8194cdec02bd46943b099d8a6392e7ae1409b8
Alejandro Martinez <hello@alejandrom.xyz>
2017-10-13 21:34:11 -0700

    completed relationship between distress and volunteer

--- hamburg/app/volunteer.php
+++ hamburg/app/volunteer.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class volunteer extends Model
+class Volunteer extends Model
 {
-    //
+	protected $table = 'volunteers';
+
+	public function distresses()
+	{
+		return $this->belongsToMany('App\distress')->withPivot('distress_id','volunteer_id');
+	}
 }
