11e800c15a472d1611b5e472605bad3b2905b124
Gabriel Alabastro <gabriel.alabastro@gmail.com>
2017-10-14 09:39:22 -0700

    Added volunteer page

--- hamburg/routes/web.php
+++ hamburg/routes/web.php
@@ -21,6 +21,16 @@ Route::get('/form', 'HomeController@form');
 //     return view('pages.emergency_form', compact('current_location'));
 // });
 
+Route::get('/volunteer', function() {
+
+    $current_location = [
+        'lat' => 33.891992,
+        'lng' => -118.373088
+    ];
+
+    return view('pages.volunteer', compact('current_location'));
+});
+
 Route::post('/formTest','EmergencyCallForm@postForm');
 
 
